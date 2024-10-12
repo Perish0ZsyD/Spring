d69b32d3b10d1e021fbbd8ff8a2ef437e843fb23
siyuanzhou <1848142281@qq.com>
2024-10-12 21:46:14 +0800

    spring env

---
+++ Spring6/spring6-first/src/main/java/com/siyuan/User.java
@@ -0,0 +1,11 @@
+package com.siyuan;
+
+/**
+ * @author Siyuan
+ * @date 2024/10/12/21:40
+ */
+public class User {
+    public void Helloword() {
+        System.out.println("hello world spring...");
+    }
+}
